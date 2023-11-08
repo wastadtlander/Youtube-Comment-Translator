162ffdc996b8c926eac24ad77306733cabb2b6b8
quinnha <cadetha@gmail.com>
2023-11-07 19:46:00 -0500

    added docstrings

--- scripts/content.js
+++ scripts/content.js
@@ -7,6 +7,10 @@ const regexEmoji =
   /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/gu;
 const commentWrapper = document.body;
 
+/**
+ * Calls the Google Translate API
+ * @param  {object} text  text element object.
+ */
 function callTranslateAPI(text) {
   return new Promise((resolve, reject) => {
     if (text) {
@@ -35,6 +39,12 @@ function callTranslateAPI(text) {
   });
 }
 
+/**
+ * Automatically translates comments on load
+ *
+ * @param  {object} mutationsList  text element object.
+ * @param  {object} observer  observer properties.
+ */
 function translateComments(mutationsList, observer) {
   for (const mutation of mutationsList) {
     if (mutation.type == "childList") {
@@ -51,7 +61,7 @@ function translateComments(mutationsList, observer) {
                 element.textContent.trim() != "" &&
                 spellcheckValue != "false" &&
                 element.textContent != " " &&
-                !element.textContent.match(regex_emoji)
+                !element.textContent.match(regexEmoji)
               ) {
                 fetch(detectURL, {
                   method: "POST",
@@ -89,6 +99,9 @@ function translateComments(mutationsList, observer) {
   }
 }
 
+/**
+ * Translates comments on click
+ */
 commentWrapper.addEventListener("click", (event) => {
   const comment = event.target.closest("yt-formatted-string");
 
@@ -102,7 +115,9 @@ commentWrapper.addEventListener("click", (event) => {
     .catch((error) => {});
 });
 
-// Observer to translate new comments on load
+/**
+ * Observer to translate new comments on load
+ */
 const observer = new MutationObserver(translateComments);
 observer.observe(document.body, {
   childList: true,
